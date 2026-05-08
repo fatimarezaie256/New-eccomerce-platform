43e68c0f95187b3b219b08fb31456e362acde8d5
fatimarezaie256 <fatimarezaie112@gmail.com>
2026-05-08 10:49:27 +0430

    products problem resolved

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,13 +1,9 @@
 export async function GetProduct() {
   const res = await fetch("http://127.0.0.1:8000/api/products");
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch products");
-  }
-
   const response = await res.json();
 
-  return response.data.data;
+  return response.data;
 }
 
 export async function getAllPrpducts(url: string) {
--- src/app/shared/products/product-list.tsx
+++ src/app/shared/products/product-list.tsx
@@ -1,41 +1,62 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import ProductCard from "./product-cart";
-import Link from "next/link";
+import { getAllPrpducts } from "../../../../lib/actions/product.action";
 
-type ProductListProps = {
-  title?: string;
-  productList?: any[];
-};
-
-export default function ProductList({
-  limit,
-  productList,
+function ProductList({
   title,
 }: {
   limit?: number;
   productList: any;
   title?: string;
 }) {
-  // const totalPage = productList.meta.last_page;
+  const [link, setLink] = useState("http://127.0.0.1:8000/api/products?page=1");
+
+  const [list, setList] = useState<any>(null);
+
+  useEffect(() => {
+    async function getData() {
+      const products = await getAllPrpducts(link);
+      setList(products);
+    }
+
+    getData();
+  }, [link]);
+
+  if (!list?.data) {
+    return null;
+  }
+
   return (
     <div className="my-10">
-      <h2 className="h2-bold mb-4">{title ? title : "Newest Arrivals"}</h2>
+      <h2 className="h2-bold mb-4">{title ? title : "New Arrivals"}</h2>
 
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {productList.length > 0 ? (
-          productList.map((product: any) => (
-            <ProductCard limit={4} product={product} key={product.id} />
-          ))
-        ) : (
-          <p>No products found.</p>
-        )}
+        {list.data.map((product: any) => (
+          <ProductCard key={product.id} product={product} limit={4} />
+        ))}
       </div>
-      <div className="w-full flex my-4 justify-center">
-        <Button>
-          {" "}
-          <Link href="/products">View All Products</Link>
-        </Button>
+
+      <div className="w-full max-w-sm mx-auto overflow-x-auto flex justify-between items-center mt-5">
+        {list?.links?.map((link: any) => (
+          <Button
+            onClick={() =>
+              setLink(
+                link.url
+                  ? link.url
+                  : `http://127.0.0.1:8000/api/products?page=${list.current_page}`,
+              )
+            }
+            key={link.label}
+            variant={link.active ? "default" : "outline"}
+            dangerouslySetInnerHTML={{ __html: link.label }}
+          />
+        ))}
       </div>
     </div>
   );
 }
+
+export default ProductList;
